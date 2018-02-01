62f6f56835d373e13c5cb370531b1a6a637d9ad5
Ct7liang <ct7liang@163.com>
2018-02-01 18:37:21 +0800

    Initial commit

---
+++ app/src/main/java/com/android/ct7liang/MainActivity.java
@@ -0,0 +1,38 @@
+package com.android.ct7liang;
+
+import android.view.View;
+import com.ct7liang.tangyuan.BasisActivity;
+
+public class MainActivity extends BasisActivity {
+
+    @Override
+    public int setLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void findView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initFinish() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+}
